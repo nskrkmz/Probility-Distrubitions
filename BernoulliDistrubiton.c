e4a60e4108094f4e9651fb05154aa6a218e674f5
Enes Korkmaz <aeneskrkmz@gmail.com>
2020-09-06 00:40:14 +0300

    düzenleme2
    header dosyasında ki çakışmaları yok etmek için fonksiyon isimleri karakterize edildi

--- BernoulliDistrubiton.c
+++ BernoulliDistrubiton.c
@@ -4,12 +4,12 @@
 
 //represantation of function
 
-float ortalama (float p);
-float varyans (float p ,float q);
-float carpiklik (float p,float q);
-float basiklik(float p ,float q);
-float standartSapma (float p, float q);
-float pmf (float p , float q, float x);
+float bernoulli_ortalama (float p);
+float bernoulli_varyans (float p ,float q);
+float bernoulli_carpiklik (float p,float q);
+float bernoulli_basiklik(float p ,float q);
+float bernoulli_standartSapma (float p, float q);
+float bernoulli_pmf (float p , float q, float x);
 
 // main baþlangýcý
 int main (void){
@@ -18,12 +18,12 @@ int main (void){
 	float x=5 ;// pmf içinn deneme sayisi 
 	printf("Basari olasiligi \t\t%f",p);
 	printf("\nbasarisizlik olasiligi \t\t%f",q);
-	printf("\nortalama \t\t\t%f",ortalama(p));
-	printf("\nvaryans \t\t\t%f",varyans(p,q));
-	printf("\nstandart sapma \t\t\t%f",standartSapma(p,q));
-	printf("\nOlasilik kutle fonsiyonu \t%f", pmf(p,q,x));
-	printf("\ncarpiklik \t\t\t%f",carpiklik(p,q));
-	printf("\nbasiklik \t\t\t%f",basiklik(p,q));
+	printf("\nortalama \t\t\t%f",bernoulli_ortalama(p));
+	printf("\nvaryans \t\t\t%f",bernoulli_varyans(p,q));
+	printf("\nstandart sapma \t\t\t%f",bernoulli_standartSapma(p,q));
+	printf("\nOlasilik kutle fonsiyonu \t%f", bernoulli_pmf(p,q,x));
+	printf("\ncarpiklik \t\t\t%f",bernoulli_carpiklik(p,q));
+	printf("\nbasiklik \t\t\t%f",bernoulli_basiklik(p,q));
 	
 	return 0 ;
 }// main fonksiyonu sonu
@@ -31,26 +31,26 @@ int main (void){
 //defination of funtions
 
 
-float ortalama (float p) {
+float bernoulli_ortalama (float p) {
 	return p ;
 }
 
-float varyans (float p ,float q){
+float bernoulli_varyans (float p ,float q){
 	return p*q ;
 }
 
-float standartSapma (float p, float q){
-	return sqrt(varyans(p,q)) ;
+float bernoulli_standartSapma (float p, float q){
+	return sqrt(bernoulli_varyans(p,q)) ;
 }
 
-float carpiklik (float p,float q) {
-	return (1-2*p)/sqrt(varyans( p ,q));
+float bernoulli_carpiklik (float p,float q) {
+	return (1-2*p)/sqrt(bernoulli_varyans( p ,q));
 }
 
-float basiklik(float p ,float q) {
-	return (1/varyans(p,q)-6) ;
+float bernoulli_basiklik(float p ,float q) {
+	return (1/bernoulli_varyans(p,q)-6) ;
 }
 
-float pmf (float p,float q , float x){
+float bernoulli_pmf (float p,float q , float x){
 	return (pow(p,x)* pow(q,1-x));
 }
