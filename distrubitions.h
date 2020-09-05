0fab895f487c51ec98f0c1553ed4c944a172dd66
Enes Korkmaz <aeneskrkmz@gmail.com>
2020-09-06 01:26:16 +0300

    Create distrubitions.h
    Eksiklikleri Düzenle

---
+++ distrubitions.h
@@ -0,0 +1,55 @@
+
+#ifndef distrubitions
+#define distrubitions
+
+float bernoulli_ortalama (float p);
+float bernoulli_varyans (float p ,float q);
+float bernoulli_carpiklik (float p,float q);
+float bernoulli_basiklik(float p ,float q);
+float bernoulli_standartSapma (float p, float q);
+float bernoulli_pmf (float p , float q, float x);
+
+float binom_ortalama (int denemeSayisi, float p);
+float binom_varyans (int denemeSayisi ,float p ,float q);
+float binom_carpiklik (float p , int denemeSayisi , float q );
+float binom_basiklik (float denemeSayisi ,float p ,float q);
+
+
+
+
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
