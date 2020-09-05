5d1fd1a51f8288c5d2dc9e27f7277f345631044f
Enes Korkmaz <aeneskrkmz@gmail.com>
2020-09-06 00:46:06 +0300

    düzenleme 2
    header dosyasındaki çakışmaları engellemek için fonksiyon isimleri karakterize edildi

--- BinomDistrubiton.c
+++ BinomDistrubiton.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 #include<math.h>
 
-float ortalama (int denemeSayisi, float p);
-float varyans (int denemeSayisi ,float p ,float q);
-float carpiklik (float p , int denemeSayisi , float q );
-float basiklik (float denemeSayisi ,float p ,float q);
+float binom_ortalama (int denemeSayisi, float p);
+float binom_varyans (int denemeSayisi ,float p ,float q);
+float binom_carpiklik (float p , int denemeSayisi , float q );
+float binom_basiklik (float denemeSayisi ,float p ,float q);
 
 int main (void){
 	
@@ -14,30 +14,30 @@ int main (void){
 	
 	printf("Basari olasiligi \t\t%f",p);
 	printf("\nbasarisizlik olasiligi \t\t%f",q);
-	printf("\nortalama \t\t\t%f",ortalama(denemeSayisi,p));
-	printf("\nvaryans \t\t\t%f",varyans(denemeSayisi,p,q));
-	printf("\ncarpiklik \t\t\t%f",carpiklik(p,denemeSayisi,q));
-	printf("\nbasiklik \t\t\t%f",basiklik(denemeSayisi,p,q));
+	printf("\nortalama \t\t\t%f",binom_ortalama(denemeSayisi,p));
+	printf("\nvaryans \t\t\t%f",binom_varyans(denemeSayisi,p,q));
+	printf("\ncarpiklik \t\t\t%f",binom_carpiklik(p,denemeSayisi,q));
+	printf("\nbasiklik \t\t\t%f",binom_basiklik(denemeSayisi,p,q));
 	
 	
 	return 0 ;
 	
 } 
 
-float ortalama (int denemeSayisi, float p){
+float binom_ortalama (int denemeSayisi, float p){
 	return denemeSayisi*p ;
 }
 
-float varyans (int denemeSayisi ,float p ,float q){
+float binom_varyans (int denemeSayisi ,float p ,float q){
 	return denemeSayisi*p*q ;
 }
 
-float carpiklik (float p , int denemeSayisi , float q ){
-	return (1-2*p)/ sqrt(varyans(denemeSayisi,p,q));
+float binom_carpiklik (float p , int denemeSayisi , float q ){
+	return (1-2*p)/ sqrt(binom_varyans(denemeSayisi,p,q));
 }
 
-float basiklik (float denemeSayisi ,float p ,float q){
-	return (1/varyans(denemeSayisi,p,q))-(6/denemeSayisi);
+float binom_basiklik (float denemeSayisi ,float p ,float q){
+	return (1/binom_varyans(denemeSayisi,p,q))-(6/denemeSayisi);
 }
 
 
